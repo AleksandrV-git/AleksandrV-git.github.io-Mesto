950a18f338d08fc34608d87e1003aa92db3d7ef4
Aleksandr Vukolov <kolomel@yandex.ru>
2020-08-21 13:52:56 +0300

    first commit

---
+++ scripts/popupImage.js
@@ -0,0 +1,7 @@
+class PopupImage extends Popup {
+  open = (imgLink) => {
+    const popupImg = this.popup.querySelector(".popup_img")
+    popupImg.setAttribute("src", `${imgLink}`);
+    this.popup.classList.add('popup_is-opened');
+  }
+}
